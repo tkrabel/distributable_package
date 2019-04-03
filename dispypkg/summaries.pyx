0b66eba3e8d70c3cabc4c71a25260825b8512f3c
tkrabel <tobiaskrabel@gmx.de>
2019-04-03 15:17:27 +0200

    formatting code

--- dispypkg/summaries.pyx
+++ dispypkg/summaries.pyx
@@ -4,8 +4,8 @@ def summary(x):
     funcs = dict(
         Minimum = np.min, 
         Maxipmum = np.max, 
-        Mean = np.mean
-    )
+        Mean = np.mean)
+    
     for label, func in funcs.items():
         value = func(x)
         print("%10s: %10.2f" % (label, value))
