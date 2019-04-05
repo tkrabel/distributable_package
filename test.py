2af8abe4a05d913b6ccc5be199cde41c51cecfbf
tkrabel <tobiaskrabel@gmx.de>
2019-04-05 13:59:50 +0200

    dev stuff

--- test.py
+++ test.py
@@ -1,15 +1,13 @@
 import sys
 import os
-
-name = "John Doe"
-age = 30
-
-# best way to write multiline strings
-string = ("%s (%s), I have some more information for you. "
-          "Click here.") % (name, age)
-print(string)
-
-for dirpath, dirnames, filenames in os.walk('dispypkg'):
-    print("%20s %20s %20s" % (dirpath, dirnames, filenames))
+import glob
 
 print(__file__)
+
+# +
+## glob pattern that searches all python files not naming X.py
+# -
+
+glob.glob('*.py')
+
+glob.glob('[!__init]*.py')
